8c2a0afc9cec7f2ceb0380327991c42070b69f03
ricardorwo <ricardorwo2023@gmail.com>
2024-05-21 00:15:04 -0300

    exercicio iniciando com Java

---
+++ PRATICA JAVA SCRIPT/Desafio Iniciando com Java/script.js
@@ -0,0 +1,85 @@
+// 1. Crie um script que exiba a mensagem "Hello World!" em um alerta no navegador.
+
+// let Hello = alert("Hello Word")
+
+// 2. Crie um script que declare duas variáveis e exiba o resultado da soma entre elas.
+
+// let numberOne = prompt("Digite o primeiro número")
+// let numberTwo = prompt("Digite o segundo número")
+// let result = Number(numberOne) + Number(numberTwo)
+
+// alert("A soma dos dois números é: " + result)
+
+// 3. Crie um script que declare uma variável e verifique se o seu valor é um número. Se for, exiba a mensagem "É um número", caso contrário, exiba a mensagem "Não é um número". 
+
+// let value = prompt('Digite algo')
+
+// if(isNaN(value)) {
+//   alert('O valor digitado não é um número')
+// } else {
+//   alert('O valor digitado é um número')
+// }
+
+// 4. Crie um script que declare uma variável e verifique se o seu valor é uma string. Se for, exiba a mensagem "É uma string", caso contrário, exiba a mensagem "Não é uma string".
+
+// let value = prompt('Digite algo')
+
+// if(isNaN(value)) {
+//   alert('O valor digitado é uma string')
+// } else {
+//   alert('Não é uma string')
+// }
+
+// 5. Crie um script que declare uma variável e verifique se o seu valor é um booleano. Se for, exiba a mensagem "É um booleano", caso contrário, exiba a mensagem "Não é um booleano".
+
+// const isBoolean = false
+
+// if (typeof isBoolean == 'boolean') {
+//   alert('É um booleano')
+// } else {
+//   alert('Não é um booleano')
+// }
+
+// 6. Crie um script que declare duas variáveis e exiba o resultado da subtração entre elas.
+
+// let numberOne = prompt('Digite o primeiro número')
+// let numberTwo = prompt('Digite o segundo número')
+// let result = Number(numberOne) - Number(numberTwo)
+
+// alert('o resultado da subtrabação entre os dois números é: ' + result)
+
+// 7. Crie um script que declare duas variáveis e exiba o resultado da multiplicação entre elas.
+
+// let numberOne = prompt('Digite o primeiro número')
+// let numberTwo = prompt('Digite o segundo número')
+// let result = Number(numberOne) * Number(numberTwo)
+
+// alert('o resultado da multiplicação entre os dois números é: ' + result)
+
+// 8. Crie um script que declare duas variáveis e exiba o resultado da divisão entre elas.
+
+// let numberOne = prompt('Digite o primeiro número')
+// let numberTwo = prompt('Digite o segundo número')
+// let result = Number(numberOne) / Number(numberTwo)
+
+// alert('o resultado da divisão entre os dois números é: ' + result)
+
+// 9. Crie um script que declare uma variável e verifique se o seu valor é um número par. Se for, exiba a mensagem "É um número par", caso contrário, exiba a mensagem "Não é um número par".
+
+// const isPar = prompt('Digite um número')
+
+// if (isPar % 2 == 0) {
+//   alert('É um número par')
+// } else {
+//   alert('Não é um número par')
+// }
+
+// 10. Crie um script que declare uma variável e verifique se o seu valor é um número ímpar. Se for, exiba a mensagem "É um número ímpar", caso contrário, exiba a mensagem "Não é um número ímpar".
+
+// const isImpar = prompt('Digite um número')
+
+// if (isImpar % 2 != 0) {
+//   alert('É um número ímpar')
+// } else {
+//   alert('Não é um número ímpar')
+// }
