26532c82bea8cc8082e30a4d5df2815ee5b7a6e2
ricardorwo <ricardorwo2023@gmail.com>
2024-05-26 14:48:49 -0300

    jogo atualizado

--- PRATICA JAVA SCRIPT/Jogo-adivinhacao/main.js
+++ PRATICA JAVA SCRIPT/Jogo-adivinhacao/main.js
@@ -1,18 +1,23 @@
+// variáveis
 const screen1 = document.querySelector(".screen1")
 const screen2 = document.querySelector(".screen2")
-
+const btnTry = document.querySelector("#btnTry")
+const btnReset = document.querySelector("#btnReset")
 const randomNumber = Math.round(Math.random() * 10)
 let xAttempts = 1
 
-// Função callback
+// Eventos
+btnTry.addEventListener('click', handleTryClick)
+btnReset.addEventListener('click', handleResetClick)
+
+// Funções callback
 function handleTryClick(event) {
   event.preventDefault() // não faça do padrão do button
 
   const inputNumber = document.querySelector("#inputNumber")
 
   if (Number(inputNumber.value) == randomNumber) {
-    screen1.classList.add("hide")
-    screen2.classList.remove("hide")
+    toggleScreen()
     screen2.innerText = `Você acertou em ${xAttempts} tentativas!`
   }
 
@@ -20,13 +25,13 @@ function handleTryClick(event) {
 
 }
 
-// Eventos
-const btnTry = document.querySelector("#btnTry")
-const btnReset = document.querySelector("#btnReset")
-
-btnTry.addEventListener('click', handleTryClick)
-btnReset.addEventListener('click', function() {
-  screen1.classList.remove("hide")
-  screen2.classList.add("hide")
+function handleResetClick() {
+  toggleScreen()
   xAttempts = 1
-})
+}
+
+function toggleScreen() {
+  screen1.classList.toggle("hide")
+  screen2.classList.toggle("hide")
+}
+
