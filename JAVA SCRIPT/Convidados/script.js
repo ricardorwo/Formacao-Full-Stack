ddbd3dee2cf490ccadc13d6b162ccbdef2d0de2d
ricardorwo <ricardorwo2023@gmail.com>
2024-08-20 11:19:08 -0300

    ok

---
+++ JAVA SCRIPT/Convidados/script.js
@@ -0,0 +1,62 @@
+//Visualizar o conteúdo do document.
+// console.log(document)
+
+//Obter o title da página.
+// console.log(document.title)
+
+//Acessar o elemento pelo ID (seletor ID).
+// const guest = document.getElementById("guest-2")
+
+//Mostra as propriedades do objeto.
+// console.dir(guest)
+
+//Acessar o elemento com class (seletor class).
+// const guestByClass = document.getElementsByClassName("guest")
+// console.log(guestByClass[0])
+
+//Selecionar lista elementos pela Tag.
+// const guestsTag = document.getElementsByTagName("h1")
+// console.log(guestsTag)
+
+//Acessa o elemento pelo seletor ID.
+// const guest = document.querySelector("#guest-1")
+// console.log(guest)
+
+//Acessa o elemento pelo seletor Class.
+// const guests = document.querySelector(".guest")
+// console.log(guests)
+
+//Retorna todos os elementos encontrados pelo query.
+// const guests = document.querySelectorAll(".guest")
+// console.log(guests)
+
+
+
+
+
+
+
+// const guest = document.querySelector("#guest-2 span")
+
+//Retorna o conteúdo como texto:
+// console.log(guest.textContent)
+
+// substitui o texto selecionado:
+// guest.textContent = "Gabriel"
+
+
+
+
+
+// const guest = document.querySelector("#guest-2")
+
+// console.log(guest.textContent) //retorna com formatação - conteúdo visível e oculto
+// console.log(guest.innerText) //retorna sem formatação - somente o conteúdo visível
+// console.log(guest.innerHTML) //retorna o HTML como texto
+
+
+
+
+// const guests = document.querySelector("ul")
+
+// const newGuest = document.createElement("li")
